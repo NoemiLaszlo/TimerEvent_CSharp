711217d66ade2bab27e4893e292629ea29d97fb3
Myna44 <mynadesign@gmail.com>
2018-09-10 11:59:18 +0200

    TimerEvents example created

--- TimerEvents/MainWindow.xaml.cs
+++ TimerEvents/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TimerEvents
 {
@@ -20,9 +21,29 @@ namespace TimerEvents
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer _timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            
+    }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Tick += new EventHandler(dispatcherTimer_Tick);
+            _timer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            myProgressBar.Value += 10;
+            if (myProgressBar.Value >= 100)
+            {
+                _timer.Stop();
+            }
         }
     }
 }
